d310bde4ba79cdde9b683c3596eaeb986e1e74ac
sjohnsonn <sjohnson24@scienceleasdersjip.org>
2023-03-24 13:54:11 -0400

    added choicebox

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -32,9 +32,38 @@ public class Main extends Application {
             Image image2 = new Image(input2);
             imageView.setImage(image2);
             Button button2 = new Button("My Button");
-
-
         });
+
+        public void start(Stage primaryStage) throws Exception {
+            primaryStage.setTitle("ChoiceBox Experiment 1");
+
+            ChoiceBox<String> choiceBox = new ChoiceBox<>();
+
+            choiceBox.getItems().add("Choice 1");
+            choiceBox.getItems().add("Choice 2");
+            choiceBox.getItems().add("Choice 3");
+
+            HBox hbox = new HBox(choiceBox);
+
+            Scene scene = new Scene(hbox, 200, 100);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        }
+
+        public static void main(String[] args) {
+            Application.launch(args);
+        }
+    }
+choiceBox.setOnAction((event) -> {
+        ChoiceBox<Object> choiceBox = new ChoiceBox<>();
+        int selectedIndex = choiceBox.getSelectionModel().getSelectedIndex();
+        Object selectedItem = choiceBox.getSelectionModel().getSelectedItem();
+
+        System.out.println("Selection made: [" + selectedIndex + "] " + selectedItem);
+        System.out.println("   ChoiceBox.getValue(): " + choiceBox.getValue());
+    });
+
 
         // Layout the controls
         VBox vbox1 = new VBox(imageView,button);
